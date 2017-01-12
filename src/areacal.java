0e8d481d49b5543dd7ea2d5d65b8bc0ccf132573
GokulGitConfig <signingokul@gmail.com>
2017-01-12 19:44:26 +0530

    New Ant Project

---
+++ src/areacal.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+ 
+public class areacal
+{
+ 
+        public static void main(String[] args) 
+		{
+               
+                int radius = 0;
+                System.out.println("Please enter radius of a circle");
+               
+                
+                        //get the radius from console
+                        radius = 20;
+                               
+				 double area = Math.PI * radius * radius;
+               
+                System.out.println("Area of a circle is " + area);
+        
+				
+			
+		}
+		
+	}
